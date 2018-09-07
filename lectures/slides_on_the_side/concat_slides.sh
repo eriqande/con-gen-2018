5361ed8ce72cb27cb6f183b826df99b17d8b27ab
Eric C. Anderson <eric.anderson@noaa.gov>
2018-09-07 10:25:40 -0600

    Initial commit

---
+++ lectures/slides_on_the_side/concat_slides.sh
@@ -0,0 +1,21 @@
+
+
+PDFconcat --output slides_on_the_side.pdf ../diagrams/slide_simple1.pdf \
+	../slide_grabs/dag-notes.pdf \
+	../diagrams/slide_infer1.pdf \
+	../figures/beta_densities.pdf \
+	../diagrams/slide_infer1-with-prior.pdf \
+	../figures/pa_posterior.pdf \
+	../slide_grabs/random-walk.pdf  \
+	../diagrams/slide_genos-and-reads-dag.pdf  \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/wifl_big_pop_no_bounds.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/simulated-nice-data.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/lobster_big_pop.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/snails_big_pop.pdf \
+	../diagrams/slide_geno-err-model.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_hwe_plot.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_eelp_afprior_plot.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_umpm_afprior_plot.pdf \
+	../figures/chin_mykiss_read_count_histos.pdf \
+	../../explorations/outputs/gatk_pca.pdf \
+	../../explorations/outputs/angsd_pca.pdf 
